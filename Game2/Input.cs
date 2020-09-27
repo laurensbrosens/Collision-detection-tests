4b8556270cb31066936323e620aa2a2a6551ca46
laurens <laurens.brosens@student.ap.be>
2020-09-27 22:26:26 +0200

    Gravity, jumping and vertical movement

--- Game2/Input.cs
+++ Game2/Input.cs
@@ -8,19 +8,24 @@ namespace Game2
 {
     class Input
     {
-        public float Inputs()
+        public Vector2 Inputs()
         {
-            float displacment = 0f;
+            float displacmentX = 0f;
+            float displacmentY = 0f;
             KeyboardState stateKey = Keyboard.GetState();
             if (stateKey.IsKeyDown(Keys.A))
             {
-                displacment += - 1f;
+                displacmentX += - 1f;
             }
             if (stateKey.IsKeyDown(Keys.D))
             {
-                displacment += 1f;
+                displacmentX += 1f;
             }
-            return displacment;
+            if (stateKey.IsKeyDown(Keys.Space))
+            {
+                displacmentY += 1f;
+            }
+            return new Vector2((int)displacmentX, (int)displacmentY);
         }
     }
 }
--- Game2/Physics.cs
+++ Game2/Physics.cs
@@ -8,22 +8,35 @@ namespace Game2
     class Physics
     {
         public float groundResistance { get; set; } = 0.15f; //Weerstand op de grond
-        public float airResistance { get; set; } = 0.04f; //Weerstand in de lucht
+        public float airResistance { get; set; } = 0.015f; //Weerstand in de lucht
         public float inputAcceleration { get; set; } = 100f; //Acceleratie door input
         //public float AccelerationX { get; set; } = 0f; //Horizontale acceleratie
-        public float Gravity { get; set; } = 0.98f; //Acceleratie door zwaartekracht
+        public float Gravity { get; set; } = 9.8f; //Acceleratie door zwaartekracht
+        public float jumpingSpeed { get; set; } = 10f; //Snelheid bij springen
         public float VelocityX { get; set; } = 0; //Snelheid horizontaal
         public float VelocityY { get; set; } = 0; //Snelheid verticaal
 
-        public float horizontalInput = 0f; //De input van de speler
+        public float horizontalInput = 0f; //De input van de speler horizontaal
+        public float verticalInput = 0f; //De input van de speler verticaal
 
         private Input PlayerInput = new Input(); //Object initialiseren om input keyboard raad te plegen
         public Vector2 Movement(GameTime gameTime)
         {
             float deltaT = (float)gameTime.ElapsedGameTime.TotalSeconds;
-            horizontalInput = PlayerInput.Inputs();
-            VelocityX += inputAcceleration * horizontalInput - groundResistance * VelocityX;
-            return new Vector2(VelocityX * deltaT, 0);
+            Vector2 playerInput = PlayerInput.Inputs();
+            horizontalInput = playerInput.X;
+            verticalInput = playerInput.Y;
+
+            VelocityX += inputAcceleration * horizontalInput - groundResistance * VelocityX; //Berekening van horizontale snelheid, snelheidx = acceleratie * input - grondweerstand * snelheidx
+            if (verticalInput != 0)
+            {
+                VelocityY = -jumpingSpeed;
+            }
+            else
+            {
+                VelocityY += VelocityY * deltaT + 50f * Gravity * deltaT * deltaT - airResistance * VelocityY; //Berekening van verticale snelheid met zwaartekracht
+            }
+            return new Vector2(VelocityX * deltaT, VelocityY);
         }
     }
 }
